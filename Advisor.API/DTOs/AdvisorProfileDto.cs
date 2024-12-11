3adab2c804c594f95861273e32f5e045d4da9bf3
Talha Mohsin <talha.mohsin@payflo.com>
2024-12-11 10:46:47 -0500

    small bug fix

--- Advisor.API/DTOs/AdvisorProfileDto.cs
+++ Advisor.API/DTOs/AdvisorProfileDto.cs
@@ -15,5 +15,5 @@ public class AdvisorProfileResponseDto
     public string? Address { get; set; }
     public string SIN { get; set; }
     public string PhoneNumber { get; set; }
-    public string HealthStatus { get; set; }
+    public string HealthStatus { get; private set; }
 }
